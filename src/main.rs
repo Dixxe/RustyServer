1c7445d0bd25140790614f66e78b896014f63bb8
Dixxe <dixxe.mail@proton.me>
2024-08-16 08:45:49 +0300

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use core::time;
+use core::{panic, str, time};
 
 use std::{
     io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, os::fd::AsFd, thread::sleep
 };
 
 pub mod packets;
+use packets::packet_base::*;
+
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3493").unwrap();
@@ -13,24 +16,37 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("connection!");
-        handle_connection(stream);
+        handle_connection(&stream);
+        game_input_stream(&stream);
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
+fn handle_connection(stream: &TcpStream) {
+    let buf_reader = BufReader::new(stream);
     dbg!(&buf_reader);
 
     
-    let mut tick_count: i32 = 0;
-    let mut buf = [0, 10];
+}
+
+fn game_input_stream(mut stream: &TcpStream) {
+    let mut buf: [u8; 1] = [0];
     loop {
-        // stream.write_all(&[0]);
-        stream.read(&mut buf[..]);
-        sleep(time::Duration::from_millis(500));
-        println!("tick {}!\n-------------------------------", tick_count);
-        dbg!(buf); // ОНО ДАЕТ ПАКЕТЫ ХАХАХЫХВЫВ
-        tick_count += 1;
+        stream.read(&mut buf[..]).unwrap(); // ОНО ДАЕТ ПАКЕТЫ ХАХАХЫХВЫВ
+        for byte in buf {
+            match byte {
+                _ => print!("Unknown packet: {} \n", byte)
+            }
+        }
+        let s = match  str::from_utf8(&buf) {
+            Ok(v) => v,
+            Err(e) => "wrong",
+        };
+        println!("{}", s);
+        sleep(time::Duration::from_millis(100));
     }
-    
+}
+
+fn game_output_strean(mut stream: &TcpStream, byte: u8) {
+    let mut buf = [byte];
+    stream.write_all(&buf).unwrap();
 }
