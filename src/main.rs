7ec3e6b99be7651afe7aea0b8daf29a2e1e69c0e
Dixxe <dixxe.mail@proton.me>
2024-08-16 09:22:54 +0300

    swap to tokio

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,41 @@
-use core::{panic, str, time};
+use core::{str, time};
 
 use std::{
-    io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, os::fd::AsFd, thread::sleep
+    io::{self, prelude::*, BufReader}, str::FromStr, thread::sleep
 };
+
+use tokio::{io::AsyncReadExt, net::{TcpListener, TcpStream}};
 
 pub mod packets;
 use packets::packet_base::*;
 
-
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3493").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        println!("connection!");
-        handle_connection(&stream);
-        game_input_stream(&stream);
-    }
-}
-
-fn handle_connection(stream: &TcpStream) {
-    let buf_reader = BufReader::new(stream);
-    dbg!(&buf_reader);
-
-    
-}
-
-fn game_input_stream(mut stream: &TcpStream) {
+async fn process_socket(mut stream: TcpStream) {
+    println!("Connected");
     let mut buf: [u8; 1] = [0];
     loop {
-        stream.read(&mut buf[..]).unwrap(); // ОНО ДАЕТ ПАКЕТЫ ХАХАХЫХВЫВ
+        stream.read(&mut buf).await.unwrap();
         for byte in buf {
             match byte {
-                _ => print!("Unknown packet: {} \n", byte)
+                _ => println!("Unknow packet: {}", byte)
             }
         }
-        let s = match  str::from_utf8(&buf) {
+        let s = match str::from_utf8(&buf) {
             Ok(v) => v,
-            Err(e) => "wrong",
+            Err(e) => "not string"
         };
-        println!("{}", s);
+        println!("{}",s);
         sleep(time::Duration::from_millis(100));
     }
 }
 
-fn game_output_strean(mut stream: &TcpStream, byte: u8) {
-    let mut buf = [byte];
-    stream.write_all(&buf).unwrap();
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let mut listener = TcpListener::bind("127.0.0.1:3493").await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        process_socket(stream).await;
+    }
 }
+
